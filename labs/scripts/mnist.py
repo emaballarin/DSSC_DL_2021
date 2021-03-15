76eccf937a62bef42565cda1382e736b294cedce
Emanuele Ballarin <emanuele@ballarin.cc>
2021-03-15 20:01:52 +0100

    ✨ Edit, add, reformat. A mess (sorry)

--- labs/scripts/mnist.py
+++ labs/scripts/mnist.py
@@ -3,7 +3,10 @@ from torch.utils.data import DataLoader
 from torchvision.datasets import MNIST
 from torchvision.transforms import ToTensor, Normalize, Compose
 
-def get_data(data_root="datasets/", batch_size_train=256, batch_size_test=512, **kwargs):
+
+def get_data(
+    data_root="datasets/", batch_size_train=256, batch_size_test=512, **kwargs
+):
     # Create the folder where the data will be downloaded
     # exist_ok avoid exception if the folder already exists
     os.makedirs(data_root, exist_ok=True)
@@ -13,10 +16,7 @@ def get_data(data_root="datasets/", batch_size_train=256, batch_size_test=512, *
     # Normalize(), instead, normalizes it w.r.t. the given mean and std. Since MNIST is grayscale,
     # we have only 1 color channel, hence, mean and std are considered as singleton tuples. If we had RGB
     # images, we should've written someting like Normalize((mean channel 1, mean channel 2, mean channel 3), ...)
-    transforms = Compose([
-        ToTensor(),
-        Normalize((0.1307,), (0.3081,))
-    ])
+    transforms = Compose([ToTensor(), Normalize((0.1307,), (0.3081,))])
 
     # We download the train and the test dataset in the given root and applying the given transforms
     trainset = MNIST(data_root, train=True, transform=transforms, download=True)
@@ -29,4 +29,3 @@ def get_data(data_root="datasets/", batch_size_train=256, batch_size_test=512, *
     testloader = DataLoader(trainset, batch_size=batch_size_test, shuffle=False)
 
     return trainloader, testloader, trainset, testset
-
