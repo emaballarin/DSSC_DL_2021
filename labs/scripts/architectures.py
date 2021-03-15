286dbe5d597477353fdd8d02a863f3e2c04877fd
Emanuele Ballarin <emanuele@ballarin.cc>
2021-03-15 20:06:58 +0100

    ✨ Post-resync reformat

--- labs/scripts/architectures.py
+++ labs/scripts/architectures.py
@@ -1,5 +1,5 @@
 import torch
 from torch import nn
 
-def MLP(nn.Module):
-    def __init__(width_layers, size_in=784, num_classes=10, activ_hidden=nn.ReLU, activ_out=None)
+class MLP(nn.Module):
+   def __init__(width_layers, size_in=784, num_classes=10, activ_hidden=nn.ReLU, activ_out=None)
