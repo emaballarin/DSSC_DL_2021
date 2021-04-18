97ea5c1dbb3a08d22d1f2e144f3ba45a00651b5e
Emanuele Ballarin <emanuele@ballarin.cc>
2021-04-18 04:52:34 +0200

    HW05 (sort of)

--- homework/HW05/imp_roved.py
+++ homework/HW05/imp_roved.py
@@ -14,14 +14,9 @@
 # IMPORTS:
 from typing import Union, Optional, Tuple, Set, TypedDict
 from torch import Tensor
-
 import torch as th
 
-# USELESS:
-from scripts import architectures
-
 # CUSTOM TYPES:
-
 realnum = Union[float, int]
 
 
@@ -47,13 +42,35 @@ def paramsplit(
     return tuple(paramname.split("."))[lfrom:lto]
 
 
-def maskdrill(odict: Mask, okey: str, ikey: str) -> Set[int]:
-    """Return the det contained in a Mask after double key lookup, empty of none"""
+def _maskdrill(odict: Mask, okey: str, ikey: str) -> Set[int]:
+    """Return the set contained in a Mask after double key lookup, empty if none"""
     if okey in odict.keys():
         if ikey in odict[okey].keys():
             return odict[okey][ikey]
         return set()
     return set()
+
+
+def mask_size(maskin: Mask) -> int:
+    """
+    Count the number of elements inside a Mask object
+
+    Parameters
+    ----------
+    maskin : Mask
+        The Mask the number of elements of is required
+
+    Returns
+    -------
+    int
+        The number of elements in such mask
+    """
+
+    ret: int = 0
+    for okey in maskin.keys():
+        for ikey in maskin[okey].keys():
+            ret += len(maskin[okey][ikey])
+    return ret
 
 
 def maskterialize(tsize, indexset: Set[int]) -> Tensor:
@@ -66,37 +83,106 @@ def maskterialize(tsize, indexset: Set[int]) -> Tensor:
 def magnitude_pruning(
     model,
     rate: realnum,
-    restrict_layers: Optional[Set[str]] = None,
-    restrict_parameters: Optional[Set[str]] = None,
+    restr_layers: Optional[Set[str]] = None,
+    restr_parameters: Optional[Set[str]] = None,
     mask: Optional[Mask] = None,
 ) -> Mask:
+    """
+    Apply one iteration of Iterative Magnitude Pruning to a PyTorch model
 
-    # Signature validation
+    Parameters
+    ----------
+    model : Any valid PyTorch model
+        The model to prune
+    rate : Union[float, int]
+        The pruning rate
+    restr_layers : Optional[Set[str]], optional
+        The number of the layers to which to restrict pruning. If None, no restriction is applied. Defaults to None.
+    restr_parameters : Optional[Set[str]], optional
+        The name of the tensor-parameters to which to restrict pruning. If None, no restriction is applied. Defaults to None.
+    mask : Optional[Mask], optional
+        A starting mask for the pruning process. Indexes denote already-pruned (or unprunable) parameters. Useful in case of repeated iterations of IMP. Defaults to None, equivalent to an empty mask.
+
+    Returns
+    -------
+    Mask
+        The pruning mask at the end of the IMP iteration.
+
+    Raises
+    ------
+    ValueError
+        In case a pruning rate incompatible with a ratio is passed.
+    """
+
+    # Validate signature
     if rate < 0 or rate > 1:
         raise ValueError(
             "Given pruning rate {} is negative or exceeds 1. Provide a valid rate!"
         )
+
+    # Fill empty elements, if None
     if mask is None:
         mask: Mask = {}
+    if restr_layers is None:
+        restr_layers = []
+    if restr_parameters is None:
+        restr_parameters = []
+
+    # First pass: look and select
+
+    sel_par = []
+
+    with th.no_grad():
+        for name, param in model.named_parameters():
+
+            layname = paramsplit(name, 1)[0]
+            parname = paramsplit(name, 1)[1]
+
+            if (not restr_layers or layname in restr_layers) and (
+                not restr_parameters or parname in restr_parameters
+            ):
+                sel_par.append(
+                    param.view(-1)[
+                        list(
+                            set(range(param.numel())).difference(
+                                _maskdrill(mask, layname, parname)
+                            )
+                        )
+                    ]
+                    .clone()
+                    .detach()
+                    .abs()
+                )
+
+        flat_par = th.cat(sel_par, dim=0).sort()[0]
+
+        # Pruning threshold
+        thresh = flat_par[int(rate * flat_par.shape[0])]
+
+        # Second pass: compare and prune
+
+        for name, param in model.named_parameters():
+
+            layname = paramsplit(name, 1)[0]
+            parname = paramsplit(name, 1)[1]
+
+            if (not restr_layers or layname in restr_layers) and (
+                not restr_parameters or parname in restr_parameters
+            ):
+                flat_param = param.view(-1)
+                tensormask = th.where(flat_param.abs() >= thresh, 1, 0)
+
+                # Apply via the tensor-mask just created
+                flat_param.data *= tensormask
+
+                # Store the indexes of pruned elements
+                if not layname in mask.keys():
+                    mask[layname] = IdxSet()
+                if not parname in mask[layname].keys():
+                    mask[layname][parname] = set()
+                mask[layname][parname].update(
+                    set((tensormask == 0).nonzero().view(-1).tolist())
+                )
 
     # Return line
     return mask
-
-
-################################################################################
-# TESTS ########################################################################
-################################################################################
-layers = [
-    {"n_in": 1, "n_out": 2, "batchnorm": False},
-    {"n_out": 3, "batchnorm": True},
-    {"n_out": 4, "batchnorm": True},
-]
-
-net = architectures.MLPCustom(layers)
-
-for name, param in net.named_parameters():
-    if paramsplit(name, 1)[0] in {"1", "2"} and paramsplit(name, 1)[1] in {
-        "bias",
-        "fdsfdsf",
-    }:
-        print(param[0])
